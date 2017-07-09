a3a3b642646785935bc46a3c32fbd7b90102339e
Faisal <hak.faisal.1@gmail.com>
2017-07-09 13:05:43 +0300

    Initial commit

---
+++ AndroidStudioProjects/TalebApp/app/src/main/java/comopenid0bznsjx7nanjlcnkwnhfxn3vlzta/google/httpsdrive/talebapp/MainActivity.java
@@ -0,0 +1,13 @@
+package comopenid0bznsjx7nanjlcnkwnhfxn3vlzta.google.httpsdrive.talebapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
